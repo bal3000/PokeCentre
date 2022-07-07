44c2fdbb3a3ddf054d2a34460a74a0e71bfb22c1
bal3000 <bal3000286@googlemail.com>
2022-07-07 12:16:05 +0100

    get pokemon details

--- api/poke-centre/handlers/get-pokemon.go
+++ api/poke-centre/handlers/get-pokemon.go
@@ -1,11 +1,41 @@
 package handlers
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/bal3000/PokeCentre/proto/pokemon"
 	"github.com/gin-gonic/gin"
 )
 
-func (app *handler) GetPokemon(c *gin.Context) {
-	number := c.Param("number")
+func paramErrorMessage(param string) error {
+	return fmt.Errorf("Please provide a valid %s", "number")
+}
 
-	c.JSON(200, number)
+func (h *handler) GetPokemon(c *gin.Context) {
+	number := c.Param("number")
+	if number == "" {
+		c.AbortWithError(http.StatusBadRequest, paramErrorMessage("number"))
+		return
+	}
+
+	conv, err := strconv.Atoi(number)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, paramErrorMessage("number"))
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 1*time.Second)
+	defer cancel()
+
+	p, err := h.pokemonClient.GetPokemon(ctx, &pokemon.GetPokemonRequest{Number: int32(conv)})
+	if err != nil {
+		fmt.Println("error occurred getting pokemon details:", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}
+
+	c.JSON(200, p)
 }
